bd866278239a5747454fab9a6319746dad99f6e0
Stefan Hojer <stefan.hojer@gmail.com>
2013-09-01 16:45:46 +0200

    fixed config installation

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 cp -a bin "$HOME"
 
-CONFDIR = "$HOME/.vmtools"
+CONFDIR="$HOME/.vmtools"
 if [ ! -e "$CONFDIR" ] ; then
     mkdir -p "$CONFDIR"
-    cp -r config "$CONFDIR"
+    cp -a config/ "$CONFDIR"
 fi
