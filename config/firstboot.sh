34dea8a82479a8bc9a9048c985edd1493220f558
Stefan Hojer <stefan.hojer@gmail.com>
2014-04-21 21:49:41 +0200

    fixed firstboot script

--- config/firstboot.sh
+++ config/firstboot.sh
@@ -5,7 +5,7 @@ apt-get update
 apt-get dist-upgrade -y
 
 # create hostkey
-dpkg-reconfigure openssh
+dpkg-reconfigure openssh-server
 
 # install chef
 curl -L https://www.opscode.com/chef/install.sh | bash
